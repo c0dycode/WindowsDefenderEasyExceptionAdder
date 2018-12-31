12645807d22df41fa648f06562ae45256eefb9e7
c0dycode <schulten_705@hotmail.com>
2018-12-31 22:48:22 +0100

    Add project files.

---
+++ WindowsDefenderEasyExceptionAdder/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsDefenderEasyExceptionAdder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.listBoxCurrentExclusions = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(110, 13);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(324, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path";
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.AllowDrop = true;
+            this.tbFilePath.Location = new System.Drawing.Point(16, 60);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.ReadOnly = true;
+            this.tbFilePath.Size = new System.Drawing.Size(383, 20);
+            this.tbFilePath.TabIndex = 4;
+            this.tbFilePath.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.tbFilePath.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // listBoxCurrentExclusions
+            // 
+            this.listBoxCurrentExclusions.AllowDrop = true;
+            this.listBoxCurrentExclusions.FormattingEnabled = true;
+            this.listBoxCurrentExclusions.Location = new System.Drawing.Point(16, 87);
+            this.listBoxCurrentExclusions.Name = "listBoxCurrentExclusions";
+            this.listBoxCurrentExclusions.Size = new System.Drawing.Size(383, 368);
+            this.listBoxCurrentExclusions.TabIndex = 5;
+            this.listBoxCurrentExclusions.SelectedIndexChanged += new System.EventHandler(this.listBoxCurrentExclusions_SelectedIndexChanged);
+            this.listBoxCurrentExclusions.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.listBoxCurrentExclusions.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(216, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 468);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listBoxCurrentExclusions);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Windows Defender Exceptions";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.ListBox listBoxCurrentExclusions;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ WindowsDefenderEasyExceptionAdder/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Management.Automation;
+using System.Windows.Forms;
+
+namespace WindowsDefenderEasyExceptionAdder
+{
+    public partial class Form1 : Form
+    {
+        private string currentPath { get; set; }
+        private List<string> currentExclusions { get; set; }
+        private string selectedItem { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetCurrentExclusions();
+        }
+
+        private void GetCurrentExclusions()
+        {
+            currentExclusions = new List<string>();
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddScript($"Get-MpPreference");
+                var result = ps.Invoke();
+                var exclusions = result.FirstOrDefault()?.Members.Where(x => x.Name == "ExclusionPath").Select(x => x.Value);
+                for (int i = 0; i < exclusions.Count(); i++)
+                {
+                    var cur = (exclusions.FirstOrDefault());
+                    if (cur != null)
+                    {
+                        foreach (var path in (cur as string[]))
+                        {
+                            this.currentExclusions.Add(path);
+                        }
+                    }
+                    this.listBoxCurrentExclusions.DataSource = null;
+                    this.listBoxCurrentExclusions.DataSource = this.currentExclusions;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using(var dialog = new OpenFileDialog())
+            {
+                dialog.ShowDialog();
+                this.currentPath = dialog.FileName;
+                this.tbFilePath.Text = this.currentPath;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(this.currentPath != string.Empty)
+            {
+                using (PowerShell ps = PowerShell.Create())
+                {
+                    ps.AddScript($"Add-MpPreference -ExclusionPath {this.currentPath}");
+                    var result = ps.Invoke();
+                }
+            }
+            GetCurrentExclusions();
+            this.tbFilePath.Text = "";
+            this.currentPath = "";
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            this.tbFilePath.Text = ((string[])e.Data.GetData(DataFormats.FileDrop, false))[0];
+            this.currentPath = this.tbFilePath.Text;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void listBoxCurrentExclusions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var lb = sender as ListBox;
+            this.selectedItem = lb.SelectedItem?.ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddScript($"Remove-MpPreference -ExclusionPath \"{this.selectedItem}\"");
+                var result = ps.Invoke();
+            }
+            GetCurrentExclusions();
+        }
+    }
+}
